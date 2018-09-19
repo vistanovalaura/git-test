07a4dc1af8ebf8beda2dbc1d9d492073c550e637
lauravistanova <lauravistanova@158.197.235.28>
2018-09-19 08:04:39 +0200

    pridany komentar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-
+		// je skoro rano :) 
+		
 		System.out.println("Hello world");
 		for (int i = 0; i < 10; i++) {
 			System.out.println("Git je super");
